7e1651668a67f57067c1fbdbcc6829b60edea463
Elizabeth Harper <foxcapade@gmail.com>
2020-08-28 13:42:11 -0400

    CollectionRepo to proper singleton.

--- src/main/java/org/veupathdb/service/osi/service/collections/CollectionRepo.java
+++ src/main/java/org/veupathdb/service/osi/service/collections/CollectionRepo.java
@@ -15,7 +15,40 @@ import org.veupathdb.service.osi.util.QueryUtil;
 
 public class CollectionRepo
 {
-  public static IdSetCollection insert(NewIdSetCollection coll)
+  private static final CollectionRepo instance = new CollectionRepo();
+
+  // ╔════════════════════════════════════════════════════════════════════╗ //
+  // ║                                                                    ║ //
+  // ║    Static Access Methods                                           ║ //
+  // ║                                                                    ║ //
+  // ╚════════════════════════════════════════════════════════════════════╝ //
+
+  public static CollectionRepo getInstance() {
+    return instance;
+  }
+
+  public static IdSetCollection insert(final NewIdSetCollection coll)
+  throws Exception {
+    return getInstance().insertRow(coll);
+  }
+
+  public static Optional < IdSetCollection > select(final long id)
+  throws Exception {
+    return getInstance().selectById(id);
+  }
+
+  public static List < IdSetCollection > select(final RecordQuery query)
+  throws Exception {
+    return getInstance().selectByQuery(query);
+  }
+
+  // ╔════════════════════════════════════════════════════════════════════╗ //
+  // ║                                                                    ║ //
+  // ║    Mockable Instance Methods                                       ║ //
+  // ║                                                                    ║ //
+  // ╚════════════════════════════════════════════════════════════════════╝ //
+
+  public IdSetCollection insertRow(final NewIdSetCollection coll)
   throws Exception {
     return new BasicPreparedReadQuery <>(
       SQL.Insert.Osi.COLLECTION,
@@ -28,7 +61,7 @@ public class CollectionRepo
     ).execute().getValue();
   }
 
-  public static Optional < IdSetCollection > select(long id)
+  public Optional < IdSetCollection > selectById(final long id)
   throws Exception {
     return new BasicPreparedReadQuery<>(
       SQL.Select.Osi.Collections.BY_ID,
@@ -38,9 +71,8 @@ public class CollectionRepo
     ).execute().getValue();
   }
 
-  public static List < IdSetCollection > select(
-    final RecordQuery query
-  ) throws Exception {
+  public List < IdSetCollection > selectByQuery(final RecordQuery query)
+  throws Exception {
     try (
       var cn = DbMan.connection();
       var ps = cn.prepareStatement(SQL.Select.Osi.Collections.BY_QUERY)
@@ -80,4 +112,5 @@ public class CollectionRepo
       }
     }
   }
+
 }
