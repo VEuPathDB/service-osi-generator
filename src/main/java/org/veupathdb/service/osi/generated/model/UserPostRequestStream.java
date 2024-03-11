a1e6e21c7e2efcafd0c376f6ccc0cfa1a9b7c8fb
Ryan Doherty <tech@conical.org>
2024-03-11 13:25:09 -0400

    Commit newly added generated files

---
+++ src/main/java/org/veupathdb/service/osi/generated/model/UserPostRequestStream.java
@@ -0,0 +1,23 @@
+
+package org.veupathdb.service.osi.generated.model;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.function.Consumer;
+
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.StreamingOutput;
+
+public class UserPostRequestStream extends UserPostRequestImpl implements StreamingOutput {
+
+  private final Consumer<OutputStream> _streamer;
+
+  public UserPostRequestStream(Consumer<OutputStream> streamer) {
+    _streamer = streamer;
+  }
+
+  @Override
+  public void write(OutputStream output) throws IOException, WebApplicationException {
+    _streamer.accept(output);
+  }
+}
